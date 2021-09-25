3f4cc2c9ac0da24656feaf7c9ba2060ed927688a
Abhishek Verma <zabhishek.verma@gmail.com>
2021-09-25 18:18:25 +0530

    React Redux Form validation Exercise: Correction

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -5,11 +5,9 @@ import { Control, Errors, LocalForm } from 'react-redux-form';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
-const minLength = (len) => (val) => !(val) || (val.length >= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
 const isNumber = (val) => !isNaN(Number(val));
 const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
-
-
 
 class Contact extends Component {
     constructor(props){
