ee9d065d52e01abb8adda369245f6f9126951cbf
Abhishek Verma <zabhishek.verma@gmail.com>
2021-10-04 15:20:02 +0530

    Redux Thunk

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Label, Col, Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, Errors, LocalForm } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -141,32 +142,46 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const Dishdetail=(props) =>{
-    console.log("Dishdetail component render is invoked");
-    if (props.dish == null) {
-        return (
-            <div></div>
-        );
-    }
-    return(
-        <div class="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>{props.dish.name}</h3>
-                    <hr />
+    if (props.isLoading) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <Loading />
                 </div>
             </div>
-            <div className="row">
-                <RenderDish dish={props.dish}/>
-                <RenderComments comments={props.comments}
-                    addComment={props.addComment}
-                    dishId={props.dish.id}/>
+        );
+    }
+    else if (props.errMess) {
+        return(
+            <div className="container">
+                <div className="row">            
+                    <h4>{props.errMess}</h4>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else if (props.dish != null) {
+        return(
+            <div class="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    <RenderDish dish={props.dish}/>
+                    <RenderComments comments={props.comments}
+                        addComment={props.addComment}
+                        dishId={props.dish.id}/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Dishdetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,7 +8,7 @@ import Footer from './FooterComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import About from './AboutComponent';
 import { connect } from "react-redux";
-import { addComment } from '../redux/ActionCreators';
+import { addComment, fetchDishes } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return{
@@ -20,7 +20,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = (dispatch) => ({
-    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment)),
+    fetchDishes: () => { dispatch(fetchDishes())}
 });
 
 class Main extends Component {
@@ -28,24 +29,33 @@ class Main extends Component {
         super(props);
     }
 
-    render(){
-        const HomePage = () =>{
-            return(
-                <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
-                promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
-                leader={this.props.leaders.filter((lead) => lead.featured)[0]}
-                />
-            );
-        }
+    componentDidMount() {
+        this.props.fetchDishes();
+    }
 
-        const DishWithId = ({match}) => {
-            return (
-                <Dishdetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
-                addComment={this.props.addComment}
+    render(){
+        const HomePage = () => {
+            return(
+                <Home 
+                    dish={this.props.dishes.dishes.filter((dish) => dish.featured)[0]}
+                    dishesLoading={this.props.dishes.isLoading}
+                    dishesErrMess={this.props.dishes.errMess}
+                    promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.props.leaders.filter((leader) => leader.featured)[0]}
                 />
             );
-        }
+          }
+
+          const DishWithId = ({match}) => {
+            return(
+                <Dishdetail dish={this.props.dishes.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                  isLoading={this.props.dishes.isLoading}
+                  errMess={this.props.dishes.errMess}
+                  comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                  addComment={this.props.addComment}
+                />
+            );
+          };
 
         return (
             <div>
